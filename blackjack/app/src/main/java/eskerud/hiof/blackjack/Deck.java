0a432229e3f1f878f202934a1278d71449ed9da7
Martin Eskerud <martin@eskerud.net>
2015-03-05 20:57:37 +0100

    i fixed shit

--- blackjack/app/src/main/java/eskerud/hiof/blackjack/Deck.java
+++ blackjack/app/src/main/java/eskerud/hiof/blackjack/Deck.java
@@ -1,10 +1,16 @@
 package eskerud.hiof.blackjack;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.lang.reflect.Field;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -16,20 +22,47 @@ import java.util.Random;
 public class Deck<T> extends ArrayList {
 
 
-    public Deck(Resources res) {
-        String[] types = {"Clubs", "Spades", "Diamonds", "Hearts"};
+    public Deck(Context con) {
+        Resources res = con.getResources();
+
+     // String[] planets = res.getStringArray(R.drawable.class.getFields());
+
+
+        String[] types = {"clubs", "spades", "diamonds", "hearts"};
+
+        int[] cards = {0,R.drawable.clubsof2,R.drawable.clubsof3,R.drawable.clubsof4,R.drawable.clubsof5,R.drawable.clubsof6,R.drawable.clubsof7,R.drawable.clubsof8,R.drawable.clubsof9,
+                R.drawable.clubsof10,R.drawable.clubsofjack,R.drawable.clubsofqueen,R.drawable.clubsofking,R.drawable.clubsoface,
+                R.drawable.heartsof2,R.drawable.heartsof3,R.drawable.heartsof4,R.drawable.heartsof5,R.drawable.heartsof6,R.drawable.heartsof7,R.drawable.heartsof8,R.drawable.heartsof9,
+                R.drawable.heartsof10,R.drawable.heartsofjack,R.drawable.heartsofqueen,R.drawable.heartsofking,R.drawable.heartsoface,
+                R.drawable.diamondsof2,R.drawable.diamondsof3,R.drawable.diamondsof4,R.drawable.diamondsof5,R.drawable.diamondsof6,R.drawable.diamondsof7,R.drawable.diamondsof8,R.drawable.diamondsof9,
+                R.drawable.diamondsof10,R.drawable.diamondsofjack,R.drawable.diamondsofqueen,R.drawable.diamondsofking,R.drawable.diamondsoface,
+                R.drawable.spadesof2,R.drawable.spadesof3,R.drawable.spadesof4,R.drawable.spadesof5,R.drawable.spadesof6,R.drawable.spadesof7,R.drawable.spadesof8,R.drawable.spadesof9,
+                R.drawable.spadesof10,R.drawable.spadesofjack,R.drawable.spadesofqueen,R.drawable.spadesofking,R.drawable.spadesoface
+        };
         //for each type of card - clubs, spades, diamonds and hearts
         for (int i = 0; i < 4; i++) {
             //create a new card, specify the type of card.
 
-            for (int j = 2; j < 15; j++) {
+            //only using 9 cards per color because RAM
+            for (int j = 1; j < 14; j++) {
                 Card card = new Card(types[i]);
+
                 if (j < 11) {
                     card.setValue(j);
-                    card.setName(Integer.toString(j));
-                    int resID = res.getIdentifier(card.getType() + "of" + card.getName() + ".png", "drawable", MainActivity.PACKAGE_NAME);
-                    Log.d("this is ResID", resID + "");
-                    Bitmap img = BitmapFactory.decodeResource(res,resID);
+                    card.setName(j+"");
+                    Log.d("test:", "i is: " + i);
+                    Log.d("test:", "j is: " + j);
+                    int index;
+                    if(i==0){
+                        index = j;
+                    }
+                    else{
+                        index= j + (i*13);
+                    }
+                    Bitmap img = BitmapFactory.decodeResource(res,cards[index]);
+                    if (!img.isMutable())
+                        img = convertToMutable(con, img);
+                    Log.d("getting card:", j+(j*i) +"");
                     card.setImage(img);
                     //card.setImage()
                 } else {
@@ -37,30 +70,31 @@ public class Deck<T> extends ArrayList {
 
                     if (j == 11) {
                         card.setName("jack");
-                        int resID = res.getIdentifier(card.getType() + "of" + card.getName() + ".png", "drawable", MainActivity.PACKAGE_NAME);
-                        Log.d("this is ResID", resID + "");
-                        Bitmap img = BitmapFactory.decodeResource(res,resID);
+                        Bitmap img = BitmapFactory.decodeResource(res,cards[j+j*i]);
+                        if (!img.isMutable())
+                            img = convertToMutable(con, img);
                         card.setImage(img);
                     } else if (j == 12) {
                         card.setName("queen");
-                        int resID = res.getIdentifier(card.getType() + "of" + card.getName() + ".png", "drawable", MainActivity.PACKAGE_NAME);
-                        Log.d("this is ResID", resID + "");
-                        Bitmap img = BitmapFactory.decodeResource(res,resID);
+                        Bitmap img = BitmapFactory.decodeResource(res,cards[j+j*i]);
+                        if (!img.isMutable())
+                            img = convertToMutable(con, img);
                         card.setImage(img);
                     } else if (j == 13) {
                         card.setName("king");
-                        int resID = res.getIdentifier(card.getType() + "of" + card.getName() + ".png", "drawable", MainActivity.PACKAGE_NAME);
-                        Log.d("this is ResID", resID + "");
-                        Bitmap img = BitmapFactory.decodeResource(res,resID);
+                        Bitmap img = BitmapFactory.decodeResource(res,cards[j+j*i]);
+                        if (!img.isMutable())
+                            img = convertToMutable(con, img);
                         card.setImage(img);
                     } else if (j == 14) {
                         card.setName("ace");
-                        int resID = res.getIdentifier(card.getType() + "of" + card.getName() + ".png", "drawable", MainActivity.PACKAGE_NAME);
-                        Log.d("this is ResID", resID + "");
-                        Bitmap img = BitmapFactory.decodeResource(res,resID);
+                        Bitmap img = BitmapFactory.decodeResource(res,cards[j+j*i]);
+                        if (!img.isMutable())
+                            img = convertToMutable(con, img);
                         card.setImage(img);
                     }
                 }
+
                 this.add(card);
             }
         }
@@ -69,6 +103,33 @@ public class Deck<T> extends ArrayList {
 
     }
 
+    public static Bitmap convertToMutable(final Context context, final Bitmap imgIn) {
+        final int width = imgIn.getWidth(), height = imgIn.getHeight();
+        final Bitmap.Config type = imgIn.getConfig();
+        File outputFile = null;
+        final File outputDir = context.getCacheDir();
+        try {
+            outputFile = File.createTempFile(Long.toString(System.currentTimeMillis()), null, outputDir);
+            outputFile.deleteOnExit();
+            final RandomAccessFile randomAccessFile = new RandomAccessFile(outputFile, "rw");
+            final FileChannel channel = randomAccessFile.getChannel();
+            final MappedByteBuffer map = channel.map(FileChannel.MapMode.READ_WRITE, 0, imgIn.getRowBytes() * height);
+            imgIn.copyPixelsToBuffer(map);
+            imgIn.recycle();
+            final Bitmap result = Bitmap.createBitmap(width, height, type);
+            map.position(0);
+            result.copyPixelsFromBuffer(map);
+            channel.close();
+            randomAccessFile.close();
+            outputFile.delete();
+            return result;
+        } catch (final Exception e) {
+        } finally {
+            if (outputFile != null)
+                outputFile.delete();
+        }
+        return null;
+    }
     private void shuffleDeck() {
         long seed = System.nanoTime();
         Collections.shuffle(this, new Random(seed));
--- blackjack/app/src/main/java/eskerud/hiof/blackjack/MainActivity.java
+++ blackjack/app/src/main/java/eskerud/hiof/blackjack/MainActivity.java
@@ -4,53 +4,41 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-
 public class MainActivity extends Activity {
-    public static String PACKAGE_NAME = "net.eskerud.martin.masterthesis";
+    public static String PACKAGE_NAME;
     private Deck<Card> myDeck;
+
 
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    String PACKAGE_NAME = getPackageName();
+        String PACKAGE_NAME = getPackageName();
         super.onCreate(savedInstanceState);
+        myDeck = new Deck<Card>(getApplicationContext());
         setContentView(new MyView(this));
-        myDeck = new Deck<Card>(getResources());
 
     }
 
     public class MyView extends View {
-
+        private float y;
+        private float x;
+        private int eventType;
+        private long lastPress;
 
         public MyView(Context context) {
             super(context);
-
-            initBitmapHashmapKeys();
-        }
-
-        private void initBitmapHashmapKeys() {
-
-
         }
 
 
@@ -64,16 +52,20 @@ public class MainActivity extends Activity {
 
         @Override
         public boolean onTouchEvent(MotionEvent e) {
-            //TODO: interpolate between current position and finger position
+            x=e.getX();
+            y=e.getY();
+            eventType = e.getAction();
+            if(eventType==MotionEvent.ACTION_UP){
 
-
-            this.invalidate();
+                evaluateHit();
+            }
             return true;
         }
 
 
         @Override
         protected void onDraw(Canvas canvas) {
+
             super.onDraw(canvas);
             Paint paint = new Paint();
             paint.setStyle(Paint.Style.FILL);
@@ -88,12 +80,40 @@ public class MainActivity extends Activity {
             paint.setColor(Color.BLACK);
             canvas.drawText("Dealer:", 50, 200, paint);
             canvas.drawText("Your hand:", 50, 800, paint);
+            canvas.drawText("Hit me!     Stand", 400, 1450, paint);
+            canvas.drawText(""+x+","+y,x,y,paint);
+            Card temp = (Card) myDeck.get(0);
+            //  temp.getImage().setHeight(50);
+            // temp.getImage().setWidth(50);
 
-            //THIS IS THE PROBLEM
-           // myDeck.get(0).
-            //draw the bitmaps to canvas
-            //canvas.drawbitmap
+            canvas.drawBitmap(temp.getImage(), 0, 0, paint);
+            this.invalidate();
+
+
         }
+
+        public void evaluateHit(){
+            if(x < 660 && x > 400 && y > 1350 && y < 1500 ){
+                hitMe();
+            }
+            else if(x < 1000 && x > 800 && y > 1350 && y < 1500 ){
+                stand();
+            }
+            else{
+                Log.d(PACKAGE_NAME,"we didn't hit shit, captain");
+            }
+        }
+
+        public void hitMe(){
+
+            Log.d(PACKAGE_NAME, "we hit the 'HIT ME' box");
+
+        }
+        public void stand(){
+            Log.d(PACKAGE_NAME, "we hit the 'STAND' box");
+        }
+
+
 
         //Clamps the value of X so the pieces aren't put off screen.
 
